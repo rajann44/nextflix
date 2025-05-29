332fb3f79a71c41a20deb7979d714a21f7b2b431
rajann44 <rajann44@gmail.com>
2025-05-29 23:01:51 +0200

    user icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,16 @@ export default async function Home() {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer">
             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
           </svg>
+          <div className="flex items-center space-x-2 cursor-pointer">
+            <img
+              src="https://rb.gy/g1pwyx"
+              alt="User"
+              className="h-8 w-8 rounded-md"
+            />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+          </div>
         </div>
       </header>
 
